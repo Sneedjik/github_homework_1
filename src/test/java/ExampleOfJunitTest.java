44368087dd1bc549fd7afce9ccdf1de10dd958d9
Sergei Volkov <Sneedjik@gmail.com>
2024-09-19 10:30:08 +0600

    added ExampleOfJunitTest

--- src/test/java/ExampleOfJunitTest.java
+++ src/test/java/ExampleOfJunitTest.java
@@ -1,10 +1,25 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
 
 public class ExampleOfJunitTest {
 
+    @BeforeAll
+    static void testBase() {
+        Configuration.baseUrl = "https://github.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
     @Test
     void searchOfJunitSample() {
-
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-body").$$("ul li a").findBy(text("Soft assertions")).click();
+        $(".markdown-body").shouldHave(text("@ExtendWith"));
     }
 
 }
