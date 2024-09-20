21f5d5e1a24b34c06175b0f5fbe0028124eb5c27
Sergei Volkov <Sneedjik@gmail.com>
2024-09-21 01:06:06 +0600

    Reformed Code

--- src/test/java/ExampleOfJunitTest.java
+++ src/test/java/ExampleOfJunitTest.java
@@ -2,9 +2,11 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 
 public class ExampleOfJunitTest {
